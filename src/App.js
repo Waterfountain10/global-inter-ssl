56b25ba25984725b763ff8c63741bee8a2dd4ba2
William Lafond <lafonwi1@corp.onespan.com>
2025-07-26 08:18:52 -0400

    refactored directories and added base url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import HomePage from './Views/HomePage'
+import React from "react";
+import "./App.css";
+import HomePage from "Views/Home/HomePage";
 
 function App() {
   return (
